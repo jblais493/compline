a088997beb4f2b1f35ab678a572fc2891681292a
yvnth <yvnth@users.noreply.github.com>
2026-08-16 18:42:02 +0530

    add themes for wezterm
    Update README.org
    
    remove readme

---
+++ wezterm/lauds.lua
@@ -0,0 +1,66 @@
+return {
+    foreground = "#1a1d21",
+    background = "#f0efeb",
+
+    cursor_bg = "#4a4d51",
+    cursor_fg = "#f0efeb",
+    cursor_border = "#4a4d51",
+
+    selection_fg = "#1a1d21",
+    selection_bg = "#d8d6d3",
+
+    scrollbar_thumb = "#7d7a75",
+
+    split = "#b8b5b0",
+
+    tab_bar = {
+        background = "#e0dcd4",
+
+        active_tab = {
+            bg_color = "#4a4d51",
+            fg_color = "#f0efeb",
+        },
+
+        inactive_tab = {
+            bg_color = "#efeeed",
+            fg_color = "#1a1d21",
+        },
+
+        inactive_tab_hover = {
+            bg_color = "#d8d6d3",
+            fg_color = "#1a1d21",
+        },
+
+        new_tab = {
+            bg_color = "#e0dcd4",
+            fg_color = "#1a1d21",
+        },
+
+        new_tab_hover = {
+            bg_color = "#d8d6d3",
+            fg_color = "#1a1d21",
+        },
+    },
+
+    ansi = {
+        "#7d7a75",
+        "#8b6666",
+        "#5a6b5a",
+        "#8b7e52",
+        "#5f5c58",
+        "#64757d",
+        "#5a6b7a",
+        "#2d2a27",
+    },
+
+    brights = {
+        "#9a9791",
+        "#8b6666",
+        "#5a6b5a",
+        "#8b7e52",
+        "#5f5c58",
+        "#64757d",
+        "#5a6b7a",
+        "#1a1d21",
+    },
+}
